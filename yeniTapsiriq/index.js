daf05899cd9bbc082aa631f9a66d364b6fbc568e
Agshin Rajabov <akorecebov@gmail.com>
2019-07-01 14:21:22 +0400

    Yeni Tapsiriq

---
+++ yeniTapsiriq/index.js
@@ -0,0 +1,26 @@
+var sekiller = ['nature.jpg','dog.jpg', 'cat.jpg','ferret.jpg'];
+// console.log('Şəkil ' + sekiller[0]);
+// console.log('Şəkil ' + sekiller[1]);
+// console.log('Şəkil ' + sekiller[2]);
+// console.log('Şəkil ' + sekiller[3]);
+
+// for(var i = 0; i < sekiller.length; i++) {
+//     console.log('Şəkil ' + sekiller[i]);
+// }
+
+// var text = 0;
+// while(text < 5) {
+//     console.log('Salam')
+//     text++;
+// }
+
+var number = 0;
+
+do {
+    console.log('My Number ' + sekiller[number]);
+    number++;
+} 
+
+while (number < sekiller.length)
+
+
